29c5f87e5504deecfeda1e61457f1838144bcc5e
Иван Новожилов <imnovogilovivan@gmail.com>
2025-03-27 20:59:24 +0300

    fix

--- Website-BMW-main/src/html/js/models.js
+++ Website-BMW-main/src/html/js/models.js
@@ -1,36 +1,17 @@
-const container = document.querySelector('.container');
-
-window.addEventListener('scroll', () => {
-    const windowHeight = window.innerHeight;
-    const containerTop = container.getBoundingClientRect().top;
-    const containerVisible = containerTop < windowHeight;
-
-    if (containerVisible) {
-        container.classList.add('show');
-    }
-});
-
-
-const container_2 = document.querySelector('.container_2');
-
-window.addEventListener('scroll', () => {
-    const windowHeight = window.innerHeight;
-    const container_2Top = container_2.getBoundingClientRect().top;
-    const container_2Visible = container_2Top < windowHeight;
-
-    if (container_2Visible) {
-        container_2.classList.add('show');
-    }
-});
-
-const container_3 = document.querySelector('.container_3');
-
-window.addEventListener('scroll', () => {
-    const windowHeight = window.innerHeight;
-    const container_3Top = container_3.getBoundingClientRect().top;
-    const container_3Visible = container_3Top < windowHeight;
-
-    if (container_3Visible) {
-        container_3.classList.add('show');
-    }
-});
+function revealOnScroll(selector) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(element => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            element.classList.add('show');
+            observer.unobserve(element);
+          }
+        });
+      });
+      observer.observe(element);
+    });
+  }
+  
+  revealOnScroll('.img'); // Наблюдатель для всех элементов с классом "car"
+  
